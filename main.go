84188bb0d96a115cbfbd98a37c1222950d0ca672
Denis <dmarkitanov@gmail.com>
2019-10-22 22:11:46 +0600

    temp commit

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi")
+		fmt.Fprintf(w, "Hi, my man")
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
