4ab69f51f9cb5b7f769b6bb1b96ac4322ec91054
Denis <dmarkitanov@gmail.com>
2020-01-04 20:21:06 +0600

    use login and refresh functions from controller

--- pkg/app.go
+++ pkg/app.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"strings"
 
 	"github.com/atqwerty/choresBackend/internal/config"
 	"github.com/atqwerty/choresBackend/internal/models"
@@ -366,37 +365,38 @@ func (app *Session) register(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Session) login(w http.ResponseWriter, r *http.Request) {
-	user := &models.User{}
-
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&user); err != nil {
-		utils.BadRequest(w, "payload is required")
-		return
-	}
-	defer r.Body.Close()
-
-	if user.Email == "" {
-		utils.BadRequest(w, "email is missing")
-		return
-	}
-	if user.Password == "" {
-		utils.BadRequest(w, "password is missing")
-		return
-	}
-
-	user, err := app.Db.Login(user.Email, user.Password)
-	if err != nil {
-		utils.ServerError(w, err)
-		return
-	}
-
-	app.CurrentUserID = user.ID
-	app.Token = user.Token
-	app.RefreshToken = user.RefreshToken
-
-	cookie := http.Cookie{Name: "Auth", Value: user.Token, Expires: user.ExpireCookie, HttpOnly: true}
-	http.SetCookie(w, &cookie)
-	utils.RespondJSON(w, http.StatusOK, user)
+	app.UserControllerInternal.Login(w, r)
+	//	user := &models.User{}
+	//
+	//	decoder := json.NewDecoder(r.Body)
+	//	if err := decoder.Decode(&user); err != nil {
+	//		utils.BadRequest(w, "payload is required")
+	//		return
+	//	}
+	//	defer r.Body.Close()
+	//
+	//	if user.Email == "" {
+	//		utils.BadRequest(w, "email is missing")
+	//		return
+	//	}
+	//	if user.Password == "" {
+	//		utils.BadRequest(w, "password is missing")
+	//		return
+	//	}
+	//
+	//	user, err := app.Db.Login(user.Email, user.Password)
+	//	if err != nil {
+	//		utils.ServerError(w, err)
+	//		return
+	//	}
+	//
+	//	app.CurrentUserID = user.ID
+	//	app.Token = user.Token
+	//	app.RefreshToken = user.RefreshToken
+	//
+	//	cookie := http.Cookie{Name: "Auth", Value: user.Token, Expires: user.ExpireCookie, HttpOnly: true}
+	//	http.SetCookie(w, &cookie)
+	//	utils.RespondJSON(w, http.StatusOK, user)
 }
 
 func validate(page http.HandlerFunc) http.HandlerFunc {
@@ -429,32 +429,33 @@ func validate(page http.HandlerFunc) http.HandlerFunc {
 }
 
 func (app *Session) refresh(w http.ResponseWriter, r *http.Request) {
-	reqToken := r.Header.Get("Authorization")
-	splitToken := strings.Split(reqToken, "Bearer ")
-	reqToken = strings.Replace(splitToken[1], "\n", "", -1)
-
-	token, err := jwt.Parse(reqToken, func(token *jwt.Token) (interface{}, error) {
-		// Don't forget to validate the alg is what you expect:
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-		}
-
-		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
-		return []byte("secret"), nil
-	})
-
-	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid && token.Raw == app.RefreshToken {
-		fmt.Println(claims["foo"], claims["nbf"])
-	} else {
-		fmt.Println(err)
-		return
-	}
-
-	cookie := http.Cookie{Name: "Auth", Value: app.Token, Expires: app.Db.GenerateCookie(), HttpOnly: true}
-	http.SetCookie(w, &cookie)
-
-	utils.RespondJSON(w, http.StatusOK, reqToken)
-	return
+	app.UserControllerInternal.Refresh(w, r)
+	//	reqToken := r.Header.Get("Authorization")
+	//	splitToken := strings.Split(reqToken, "Bearer ")
+	//	reqToken = strings.Replace(splitToken[1], "\n", "", -1)
+	//
+	//	token, err := jwt.Parse(reqToken, func(token *jwt.Token) (interface{}, error) {
+	//		// Don't forget to validate the alg is what you expect:
+	//		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+	//			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+	//		}
+	//
+	//		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+	//		return []byte("secret"), nil
+	//	})
+	//
+	//	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid && token.Raw == app.RefreshToken {
+	//		fmt.Println(claims["foo"], claims["nbf"])
+	//	} else {
+	//		fmt.Println(err)
+	//		return
+	//	}
+	//
+	//	cookie := http.Cookie{Name: "Auth", Value: app.Token, Expires: app.Db.GenerateCookie(), HttpOnly: true}
+	//	http.SetCookie(w, &cookie)
+	//
+	//	utils.RespondJSON(w, http.StatusOK, reqToken)
+	//	return
 }
 
 func errorValidator(err error, errorCause int) {
